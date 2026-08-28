bdbd19e77ad5fb80bb25ec53b4fe79fe5fa676b5
Alec <alek.rmancebo@gmail.com>
2026-08-28 20:34:45 +0200

    fix 2026 bailando destinos

--- src/content/siteContent.js
+++ src/content/siteContent.js
@@ -190,7 +190,7 @@ const archiveProjectsByLanguage = {
 		{ id: '07', category: 'producto', title: 'OMEGA TRAICIÓN', year: '2024', disciplines: 'PRODUCTO, GRÁFICO', span: 1, images: ['/archivo/Omega-traicion/OT - 0.png'], projectUrl: "https://www.youtube.com/watch?v=hme6EHy2FaA" },
 		{ id: '08', category: 'UX/UI', title: 'MINDSCAPE', year: '2026', disciplines: 'UX/UI', span: 1, images: ['/archivo/Mindscape thumbnail.png'], projectUrl:"https://www.figma.com/proto/L8JY6OzlmmkAdTavQ6jGUa/Design-sprint?node-id=655-14990&viewport=47%2C163%2C0.1&t=O9pS3M503JboJgUS-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=122%3A73&show-proto-sidebar=1&page-id=38%3A2" },
 		{ id: '09', category: 'fotografía', title: 'GENDERTRASH', year: '2023', disciplines: 'FOTOGRAFÍA', span: 1, images: ['/archivo/En proceso.png'] },
-		{ id: '10', category: 'web', title: 'BAILANDO DESTINOS', year: '20264', disciplines: 'WEB', span: 1, images: ['/archivo/En proceso.png'] },
+		{ id: '10', category: 'web', title: 'BAILANDO DESTINOS', year: '2026', disciplines: 'WEB', span: 1, images: ['/archivo/En proceso.png'] },
 		{ id: '12', category: 'web', title: 'Z-TAROT', year: '2024', disciplines: 'WEB', span: 1, images: ['/archivo/Z-tarot thumbnail.png'], projectUrl: 'https://z-tarot.netlify.app/' },
         { id: '13', category: 'fotografía', title: 'RAVEN', year: '2022', disciplines: 'FOTOGRAFÍA', span: 1, images: ['/archivo/RAVEN thumbnail.png'] },
         { id: '14', category: 'fotografía', title: 'MOONDANCE', year: '2021', disciplines: 'FOTOGRAFÍA', span: 1, images: ['/archivo/Fotogramas/Fotograma 0.png'] },
